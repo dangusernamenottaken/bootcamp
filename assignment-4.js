9cdca92000e16ed8afddd832a7bf2bba7d2b53e9
dangusernamenottaken <daniel.guberman@cix.csi.cuny.edu>
2022-01-07 09:07:00 -0500

    assignment 4

---
+++ assignment-4.js
@@ -0,0 +1,34 @@
+
+document.getElementById("right").style.cssFloat = "right"
+document.getElementById("left").style.cssFloat = "justify"
+let z;
+function wow(){
+    let e = document.createElement("p");
+    let t = document.createTextNode("I'm right")
+    e.appendChild(t)
+    document.body.appendChild(e)
+    z = 0;
+    
+}
+
+function woww(){
+    let e = document.createElement("p");
+    let t = document.createTextNode("No, I'm right!")
+    e.appendChild(t)
+    document.body.appendChild(e)
+    z = 1;
+}
+
+if(document.getElementById("exampleInputPassword1")!=12345678){
+    alert("Wrong...!")
+}
+else if(pas===12345678){
+    document.querySelector("h1") = "changed"
+}
+
+
+
+
+
+    
+
