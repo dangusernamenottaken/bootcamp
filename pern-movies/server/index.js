b040efa4be44b0d17944daf1804667cefbdb9ca2
dangusernamenottaken <daniel.guberman@cix.csi.cuny.edu>
2022-01-18 10:30:39 -0500

    wow

---
+++ pern-movies/server/index.js
@@ -0,0 +1,9 @@
+const express = require("express") //requiring express module
+
+const app = express();
+const cors = require("cors")
+
+app.listen(8000,() => {
+
+    console.log("server has started on port 5000")
+})
