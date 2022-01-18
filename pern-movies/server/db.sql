17b641b5d9f844ef29d5dc5a1896853a721b84a2
dangusernamenottaken <daniel.guberman@cix.csi.cuny.edu>
2022-01-18 16:31:14 -0500

    wow

---
+++ pern-movies/server/db.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE pernMovies;
+CREATE TABLE movies (
+    movie_id SERIAL PRIMARY KEY,
+    description VARCHAR(255)
+);
+
+-- Create DATABASE pizzadatabase;
+
+-- CREATE TABLE pizzatable {
+--     topping_id SERIAL PRIMARY KEY,
+--     topping VARCHAR(255)
+-- }
+-- SELECT * FROM pizzatable
+-- //
