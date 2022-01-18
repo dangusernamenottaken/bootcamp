dbe288629e2a77be90e29ff39c0e273848b877ca
dangusernamenottaken <daniel.guberman@cix.csi.cuny.edu>
2022-01-18 14:11:10 -0500

    W

--- pern-movies/server/index.js
+++ pern-movies/server/index.js
@@ -1,8 +1,59 @@
+
 const express = require("express") //requiring express module
 
 const app = express();
 const cors = require("cors")
+const pool = require("./db")
+const PORT = 8000;
 
+app.use(cors());
+app.use(express.json());
+app.post("/pizzas",async (req,res)=>{
+    try{
+      //  {"topping":"cheese"}
+const {topping} = reg.body; 
+const newTopping = await pool.query("INSERT INTO pizzatable (topping) VALUES ($1) RETURNING *",[topping])
+  res.json(newTopping.rows[0])
+}catch(err){
+        console.error(err.message)
+    }
+})
+app.get("/pizzas", async (req,res)=>{
+    try{
+        const allTopings = await pool.query("SELECT * FROM pizzatable");
+        res.json(allToppings.rows)
+    }
+    catch(err){
+        console.error(err.message)
+    }
+})
+//get one data
+app.get("/pizzas/::id", async (req,res)=>{
+  try{
+     console.log(req.params)
+     const { id } = req.params
+     const oneData = await pool.query("SELECT * FROM pizzatable WHERE topping id = $1",[id])
+     res.joson(oneData,rows[0])
+  }
+  catch(err){
+      console.error(err.message)
+  }
+})
+//Update 
+//id,column name,newdata
+app.put("/pizzas:id",async (req,rest)=>{
+    try{
+
+        const { id } = req.params;
+        const {topping} = req.body;
+        const updateDate = await pool.query("UPDATE pizzatable SET topping id = $1 WHERE topping = $2",[id,topping])
+        rest.json("Topping was updated")
+
+    }
+    catch(err){
+        console.error(err.message)
+    }
+})
 app.listen(PORT,() => {
 
     console.log(`server has started on port ${PORT}`)
