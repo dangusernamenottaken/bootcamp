ad89863d844f8a3b41414e1afe8cd454cd945418
dangusernamenottaken <daniel.guberman@cix.csi.cuny.edu>
2022-01-19 22:00:15 -0500

    debit credit

---
+++ bank-of-react1/src/Credits.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+function Credits(props){
+  return(
+      <div>
+          Credits:
+      </div>
+  );
+  }
+
+  export default Credits;
---
+++ bank-of-react1/src/Debits.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+
+function Debits(props){
+   return(
+<div>
+  Debits: 
+</div>
+
+   );
+
+}
+export default Debits;
